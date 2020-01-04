7b0f84f02a6b301175cfcbb7f1f985722551d765
xebecproject <gorocoin@gmail.com>
2020-01-04 17:03:42 +0100

    xebec

--- INSTALL.md
+++
@@ -1,5 +0,0 @@
-Building Xebec
-=============
-
-See doc/build-*.md for instructions on building the various
-elements of the Xebec Core reference implementation of Xebec.
--- contrib/auto_gdb/xebec_dbg.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# use testnet settings,  if you need mainnet,  use ~/.xebeccore/xebecd.pid file instead
-xebec_pid=$(<~/.xebeccore/testnet3/xebecd.pid)
-sudo gdb -batch -ex "source debug.gdb" xebecd ${xebec_pid}
